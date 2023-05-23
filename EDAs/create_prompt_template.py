f7d083868faa41d1c0224dc3103ee5093f287e00
yojulab <otter.oh@gmail.com>
2023-05-23 14:17:11 +0900

    modify prompt template

--- EDAs/create_prompt_template.py
+++ EDAs/create_prompt_template.py
@@ -10,10 +10,10 @@ prompt_template_first = {
         "### Instruction(명령어):\n{instruction}\n\n### Input(입력):\n{input}\n\n### Response:\n"
     ),
     "prompt_no_input": (
-        "Below is an instruction that describes a task.\n"
-        "아래는 작업을 설명하는 명령어입니다.\n\n"
-        "Write a response that appropriately completes the request.\n"
-        "명령어에 따른 요청을 적절히 완료하는 응답을 작성하세요.\n\n"
+        "Below is the description of the error.\n"
+        "아래는 오류을 설명입니다.\n\n"
+        "Write corrections for errors.\n"
+        "오류에 대한 수정 사항을 작성하세요.\n\n"
         "### Instruction(명령어):\n{instruction}\n\n### Response:\n"
     ),
     "response_split": "### Response:",
@@ -25,14 +25,14 @@ prompt_template_second = {
         "Review the following Java code and identify any potential runtime error(s).\n"
         "다음 Java 코드를 검토하고 잠재적인 런타임 오류를 식별하십시오.\n\n"
         "Suggest the necessary modification(s) to avoid the error(s) and explain the reason behind the potential error(s) and the correction(s)\n"
-        "오류를 방지하기 위해 필요한 수정을 제안하고 잠재적 오류 및 수정 이면의 이유를 설명하십시오 \n\n"
+        "오류를 방지하기 위해 필요한 수정을 제안하고 잠재적 오류 및 수정 이유를 설명하십시오 \n\n"
         "### Instruction(명령어):\n{instruction}\n\n### Input(입력):\n{input}\n\n### Response:\n"
     ),
     "prompt_no_input": (
-        "Below is an instruction that describes a task.\n"
-        "아래는 작업을 설명하는 명령어입니다.\n\n"
-        "Write a response that appropriately completes the request.\n"
-        "명령어에 따른 요청을 적절히 완료하는 응답을 작성하세요.\n\n"
+        "Below are potential runtime error descriptions.\n"
+        "아래는 잠재적인 런타임 오류 기술입니다.\n\n"
+        "Write a response that completes any necessary corrections to prevent errors.\n"
+        "오류를 방지하기 위해 필요한 수정을 완료하는 응답을 작성하세요.\n\n"
         "### Instruction(명령어):\n{instruction}\n\n### Response:\n"
     ),
     "response_split": "### Response:",
@@ -48,19 +48,19 @@ prompt_template_third = {
         "### Instruction(명령어):\n{instruction}\n\n### Input(입력):\n{input}\n\n### Response:\n"
     ),
     "prompt_no_input": (
-        "Below is an instruction that describes a task.\n"
-        "아래는 작업을 설명하는 명령어입니다.\n\n"
-        "Write a response that appropriately completes the request.\n"
-        "명령어에 따른 요청을 적절히 완료하는 응답을 작성하세요.\n\n"
+        "Below is a description of the operation.\n"
+        "아래는 작업을 설명하는 내용입니다.\n\n"
+        "Inspect your code, identify logical errors that exist, and write comments about the errors and fixes.\n"
+        "코드를 검사하고 존재하는 논리적 오류를 식별하고 오류 및 수정에 대한 설명을 작성하세요.\n\n"
         "### Instruction(명령어):\n{instruction}\n\n### Response:\n"
     ),
     "response_split": "### Response:",
 }
 
 # save path
-datas_filepath = 'Datas/custom.json'
+datas_filepath = 'Datas/prompt_template.json'
 
-# upload templates/custom.json
+# upload templates/prompt_template.json
 prompt_template = prompt_template_first
 with open(datas_filepath, 'w', encoding='utf-8') as f:
     json.dump(prompt_template, f, ensure_ascii=False)
