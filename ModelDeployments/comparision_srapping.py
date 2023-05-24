1b31f5f615d8c6926591d9722d3171bd7ce827a0
Sanghun Oh <otter.oh@gmail.com>
2023-05-24 21:53:03 +0900

    add some files

---
+++ ModelDeployments/comparision_srapping.py
@@ -0,0 +1,5 @@
+# Refer : https://huggingface.co/spaces/tloen/alpaca-lora
+# selenium python 코드 작성
+# - 엑셀 파일 pandas로 읽기
+# - https://huggingface.co/spaces/tloen/alpaca-lora 사이트를 연다
+# - #component-0 > .svelte-drgfj5 > .scroll-hide.svelte-drgfj5
